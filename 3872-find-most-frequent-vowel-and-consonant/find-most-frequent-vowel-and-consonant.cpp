4027235b01240c9cdf19d1d3d404d0fae5319b2b
Shivaganga-27 <1nc23ec078@ncetmail.com>
2026-03-13 17:07:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (56.16%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxFreqSum(string s) {
+        map<char , int>freq;
+        for(int i=0;i<s.size();i++){
+            freq[s[i]] +=1;
+        }
+        int fv=0,fc=0;
+        for(auto a:freq){
+            if(a.first=='a'||a.first=='e'||a.first=='i'||a.first=='o'||a.first=='u'){
+                fv=max(fv,a.second);
+            }else{
+                fc=max(fc,a.second);
+            }
+        }
+            return fc+fv;
+        
+    }
+};
+            
