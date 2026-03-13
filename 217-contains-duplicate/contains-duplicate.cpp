a8d29321284459e28baccbad38d2ac8109a0d49a
Shivaganga-27 <1nc23ec078@ncetmail.com>
2026-03-13 17:08:52 +0530

    Time: 50 ms (51.96%) | Memory: 90.7 MB (59.17%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       int n=nums.size();
+       unordered_set<int>mySet;
+       for(int i=0;i<n;i++){
+        if(mySet.find(nums[i])!=mySet.end()){
+            return true;
+       }
+       mySet.insert(nums[i]);
+    }
+    return false;
+}
+};
+        
+    
